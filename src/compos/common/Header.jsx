04838e7e04a9b98e44af63b1aad23c37d35dcbc8
webb-orbit <webbenemies@gmail.com>
2024-09-21 01:15:56 +0530

    update header

--- src/compos/common/Header.jsx
+++ src/compos/common/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
             <h1 className=' w-fit text-[1.9rem] font-bold mulish tracking-wide text-transparent bg-clip-text bg-gradient-to-r from-white to-zinc-400 uppercase select-none '>hi i makarov</h1>
 
             <div className=' py-24 max-sm:py-10'>
-                <h3 className=' text-center text-neutral-200 selection:text-black oswald selection:bg-white text-[4rem] font-medium max-sm:text-[4rem] uppercase'>nothing adsolute <br /> nothing ramdom </h3>
+                <h3 className=' text-center text-neutral-200 selection:text-black oswald selection:bg-white text-[4rem] font-medium max-sm:text-[4rem] uppercase'>everything adsolute <br /> everything ramdom </h3>
             </div>
             <ShootingStars />
             <StarsBackground starDensity={0.001} />
