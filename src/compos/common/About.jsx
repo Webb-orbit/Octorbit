e02d178485121718a4577d8678fec0e215a50674
webb-orbit <webbenemies@gmail.com>
2024-10-30 20:25:41 +0530

    UPDATE ABOUT

--- src/compos/common/About.jsx
+++ src/compos/common/About.jsx
@@ -6,29 +6,28 @@ import xlogo from "../../files/xlogow.png"
 const About = () => {
 
 const abouts = `---
-**Hello! First of all, my real name isn't Makarov; it's just my nickname. **
+**Hello! First of all, my real name isn't Makarov; it's just my nickname.**
 
-I am a front-end web developer with 1.7 years of experience. I began my journey by learning HTML, CSS, and JavaScript. **Interestingly**, it took me around 9-10 months to feel little confident in writing JavaScript a long but rewarding process. 
-Now, I'm expanding my skills by learning **backend development** and building simple websites.
+I am makarov. how still creating our Identity.
 
-**Why did I start writing blogs?** Ahh, one day, I went to my relative's house to work on a website, mainly because they had Wi-Fi !. 
-While I was there, a question randomly popped into my head: ** Who is the founder of ChatGPT? ** 
+**Why did I start writing?** Ahh, one day, I went to my relative's house to work on a website, mainly because they had Wi-Fi !. 
+While I was there, a question randomly popped into my mind: ** Who is the founder of ChatGPT? ** 
 I Googled it and found out it was **Sam Altman.**
 That discovery led me to his blog, where I ended up reading **around 10 blogs** in one sitting. 
-My head was practically exploding with a **headache** from all the reading,  but it sparked a thought: 
+My head was practically exploding with a **headache** from all the reading,  but it sparked a **thought**: 
 
-what's happend I started writing blogs? 
-And why not create a self-made blog website to known thet!
+what's happend I started writing? 
+And why not create a self-made writing website to known thet!
 
 ---
-i write not because i like it.
 i write not because i want likes.
-i write not because i impressed someone.
+i write not because i impress someone.
+i write not because...
 
-i write because i express yourself.
-i write because i express emotions.
-i write because i will see myself becomeing 2.0.
-
+i write because i like it!
+i write because i express myself!
+i write because i express Thoughts!
+i write because i will see changing myself!
 ---
 
 
