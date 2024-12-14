e1398bfe79eaedd1973b051322fe844bccd352ab
Webb orbit <130279128+Webb-orbit@users.noreply.github.com>
2024-12-14 18:08:06 +0530

    Update

--- src/compos/auth/Login.jsx
+++ src/compos/auth/Login.jsx
@@ -3,16 +3,19 @@ import Admin from "../../appwrite/auth";
 
 const Login = () => {
 const [password, setpassword] = useState("")
+  const [error, seterror] = useState(false)
   const ligin = async()=>{
     try {
+seterror(false)
       const login = Admin.loginadmin("webbenemies@gmail.com", password)
     } catch (error) {
+seterror(true)
       console.log(error);
     }
   }
   return (
     <div>
-      <input type="text" onChange={(e)=> setpassword(e.target.value)} value={password}/>
+      <input type="text" className={`text-black bg-zinc-500 ${error && "border-2 border-red-500"}`} onChange={(e)=> setpassword(e.target.value)} value={password}/>
       <button onClick={ligin}>submit</button>
       hello
     </div>
