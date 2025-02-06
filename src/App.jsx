c243fac8c7c07c5cebc22508d4c112ffaa6222ea
Webb orbit <130279128+Webb-orbit@users.noreply.github.com>
2025-02-06 19:17:24 +0530

    Update App.jsx and add speech

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"
 import Admin from "./appwrite/auth"
-import { useDispatch } from "react-redux"
+import { useDispatch, useSelector} from "react-redux"
 import { storelogin } from "./store/adminslice"
 import Loadingpage from "./compos/utiles/Loadingpage"
 import { Outlet } from "react-router-dom"
@@ -9,7 +9,8 @@ import { Outlet } from "react-router-dom"
 const App = () => {
   const disptch = useDispatch()
   const [loading, setloading] = useState(true)
-
+  const {isadmin} = useSelector(state=> state.admin)
+  
   useEffect(() => {
     ; (async () => {
       try {
@@ -24,6 +25,37 @@ const App = () => {
     }
     )()
   }, [])
+
+  useEffect(() =>{
+    const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+    const recognition = new SpeechRecognition();
+    recognition.continuous = true;
+    recognition.interimResults = false;
+    recognition.lang = "en-US";
+
+    if(isadmin){
+
+    recognition.onresult = (event) => {
+      const command = event.results[event.results.length - 1][0].transcript.toLowerCase();
+      console.log("Command received:", command);
+
+      if (command === "open") {
+        window.open("https://www.youtube.com/");
+      } else if (command === "open about") {
+        window.open("https://www.github.com");
+      }
+    };
+
+    recognition.onend = () => {
+      console.log("Recognition ended, restarting...");
+      recognition.start();
+    };
+
+    recognition.start();
+    }
+
+    return () => recognition.stop();
+  },[])
 
   return loading ? (<Loadingpage />) : (
     <>
