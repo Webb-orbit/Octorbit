8958c9fdf75bbd32da5234d4d849456fc2dff22d
Webb orbit <130279128+Webb-orbit@users.noreply.github.com>
2025-02-08 16:15:39 +0530

    Update Login.jsx

--- src/compos/auth/Login.jsx
+++ src/compos/auth/Login.jsx
@@ -1,14 +1,16 @@
-
 import { useEffect, useState } from "react"
 import Admin from "../../appwrite/auth";
 
 const Login = () => {
 const [password, setpassword] = useState("")
-  const ligin =()=>{
+ const [email, setemail] = useState("")
+  const ligin = async()=>{
+    const data = await Admin.loginadmin(email, password)
   }
   return (
-    <div>
-      <input type="text" onChange={(e)=> setpassword(e.target.value)} value={password}/>
+    <div className="flex items-center justify-center gap-4 ">
+      <input className="text-neutral-900 bg-neutral-100" type="text" placeholder="password" onChange={(e)=> setpassword(e.target.value)} value={password}/>
+<input className="text-neutral-900 bg-neutral-100" type="text" placeholder="email" onChange={(e)=> setemail(e.target.value)} value={email}/>
       <button onClick={ligin}>submit</button>
     </div>
   )
