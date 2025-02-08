e457ee1bcedb85202e87976cc46fc150a0806e4b
Webb orbit <130279128+Webb-orbit@users.noreply.github.com>
2025-02-08 13:34:06 +0530

    Create Hidra.jsx

---
+++ src/compos/utiles/Hidra.jsx
@@ -0,0 +1,40 @@
+import React, { useState, useEffect} from "react";
+
+function Hidra({catch}) {
+  const [state, setstate] = useState(localStorage.getItem("ultra")==true?true:false);
+  const [random, setrandom] = useState(true)
+
+  useEffect(()=>{
+    let num = "";
+    for (let i = 0; i < 3; i++) {
+        num += Math.floor(Math.random() * 10);
+    }
+    setrandom(num)
+    return()=>{
+      num = ""
+    }
+  },[])
+
+  useEffect(()=>{
+if(passcode == `TN${random * 2}`){
+ localStorage.removeItem("ultra");
+setstate(false)
+}
+  },[passcode])
+  
+  useEffect(()=>{
+    setstate(localStorage.getItem("ultra")==true?true:false)
+  },[catch])
+
+  return state?(
+    <div className={`bg-neutral-900 fixed top-0 left-0 w-full h-screen text-neutral-200`}>
+      <div class="flex items-center justify-center h-screen">
+        <input type="text" value={passcode} onChange={(e)=> setpasscode(e.target.value)}
+          className="font-medium text-[0.9rem]"
+          />
+      </div>
+    </div>
+  ):null;
+}
+
+export default Hidra;
