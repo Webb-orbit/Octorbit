ce46d3d65892feade1e689fb17ec02a7d988f26a
Webb orbit <130279128+Webb-orbit@users.noreply.github.com>
2025-02-08 20:33:07 +0530

    Update

--- src/compos/utiles/Hidra.jsx
+++ src/compos/utiles/Hidra.jsx
@@ -37,8 +37,10 @@ if(passcode == `TN${random * 2}`){
 
   return state?(
 <div className="bg-neutral-900 z-[1000] fixed top-0 left-0 w-[100%] h-screen text-neutral-200">
-  <div className="flex flex-col gap-2 justify-center items-center h-screen w-full">
-  <p className="font-medium uppercase text-neutral-200 ">{random}</p>
+  <div className="flex flex-col gap-4 justify-center items-center h-screen w-full">
+    <h3 className="text-neutral-200 text-[1rem] uppercase">yo! don't forget who I am.<sub>TN</sub></h3>
+    <div className="flex flex-col gap-2 justify-center items-center">
+      <p className="font-medium uppercase text-neutral-200 ">{random}</p>
       <input
         type="text"
         value={passcode}
@@ -46,6 +48,7 @@ if(passcode == `TN${random * 2}`){
         className="font-medium w-[90%] p-2 bg-neutral-200 text-[0.9rem] text-black rounded-md"
         placeholder="Enter passcode"
       />
+      </div>
     </div>
     </div>
   ):null;
