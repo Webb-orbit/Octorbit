b8f42566ac97f90a4be992044e0dc0658067095e
Webb orbit <130279128+Webb-orbit@users.noreply.github.com>
2025-02-10 11:28:26 +0530

    Phone in admin view

--- src/compos/dashbord/Adminview.jsx
+++ src/compos/dashbord/Adminview.jsx
@@ -1,13 +1,17 @@
 import { Blog } from '../common/Blog'
 import Blogslist from '../common/Blogslist'
+import { Width } from '../compos/utiles/Width'
+
 
 const Adminview = () => {
     return (
+        <Width>
         <div className='flex flex-col gap-16'>
             <Blog />
             <div className=' w-full h-[1px] bg-neutral-500'></div>
             <Blogslist header={false} />
         </div>
+            <width/>
     )
 }
 
