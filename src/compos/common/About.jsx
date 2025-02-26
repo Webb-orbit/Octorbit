5cf31184468388e46758bff4b5dee707200ef821
Webb orbit <130279128+Webb-orbit@users.noreply.github.com>
2025-02-26 10:57:22 +0530

    Update About.jsx

--- src/compos/common/About.jsx
+++ src/compos/common/About.jsx
@@ -5,11 +5,11 @@ import xlogo from "../../files/xlogow.png"
 
 const About = () => {
 
-    const abouts = `
+const abouts = `
+    
+Hello! First of all, my real name isn't Makarov; it's just my nickname.
 
-**Hello! First of all, my real name isn't Makarov; it's just my nickname.**
-
-I am makarov. who still creating our Identity.
+I am makarov. who still creating own Identity.
 
 **Why did I start writing?** Ahh, one day, I went to my relative's house to work on a website, mainly because they had Wi-Fi !. 
 While I was there, a question randomly popped into my mind: ** Who is the founder of ChatGPT? ** 
@@ -17,19 +17,15 @@ I Googled it and found out it was **Sam Altman.**
 That discovery led me to his blog, where I ended up reading **around 10 blogs** in one sitting. 
 My head was practically exploding with a **headache** from all the reading,  but it sparked a **thought**: 
 
-what's happend I started writing? 
-And why not create a self-made writing website to known thet!
+what's happend if I start writing? 
 
 ---
 i write not because i want likes.
 i write not because i impress someone.
-i write not because...
 
 i write because i like it!
 i write because i express myself!
-i write because i express Thoughts!
 i write because i will see changing myself!
-
 
 
 `
