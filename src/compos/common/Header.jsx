ec864bb8e894674d664dbb60216c7ee591f02a03
Webb orbit <130279128+Webb-orbit@users.noreply.github.com>
2025-03-22 19:35:58 +0530

    Update Header.jsx

--- src/compos/common/Header.jsx
+++ src/compos/common/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
             <h1 className=' w-fit text-[1.9rem] font-bold source-sans tracking-wide text-transparent bg-clip-text bg-gradient-to-r from-white to-zinc-400 uppercase select-none max-sm:text-center max-sm:w-full '>hi i makarov</h1>
 
             <div className=' py-24 max-sm:h-full max-sm:flex max-sm:flex-col max-sm:justify-center max-sm:items-center max-sm:py-2 max-sm:absolute max-sm:top-0 max-sm:right-0 max-sm:left-0 max-sm:bottom-0'>
-                <h3 className=' text-center text-neutral-200 selection:text-black source-sans selection:bg-white text-[4rem] font-medium max-sm:text-[2rem] uppercase'>everything adsolute <br /> everything ramdom </h3>
+                <h3 className=' text-center text-neutral-200 selection:text-black source-sans selection:bg-white text-[4rem] font-medium max-sm:text-[2rem] uppercase'>Anything adsolute <br /> anything ramdom </h3>
             </div>
             <ShootingStars />
             <StarsBackground starDensity={0.001} />
