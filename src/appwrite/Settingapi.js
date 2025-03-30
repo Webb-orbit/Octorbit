32fd9126e873369d86b4645be6075bcc9c8e1d42
Webb orbit <130279128+Webb-orbit@users.noreply.github.com>
2025-03-30 11:56:22 +0530

    Setup

--- src/appwrite/Settingapi.js
+++ src/appwrite/Settingapi.js
@@ -1,2 +1,18 @@
 import { Client, Databases, ID, Query } from "appwrite";
 import Appwriteconf from "./appwrite.config";
+
+class adminsett {
+  client;
+  sett;
+
+  constructor(){
+        this.client = new Client()
+        this.client
+        .setEndpoint(Appwriteconf.appwriteendpoint)
+        .setProject(Appwriteconf.projectid);
+
+        this.sett = new Databases(this.client)
+  }
+}
+const Settbase = new adminsett()
+export default Settbase
