4537b0e1c5a6780c1ae0f9b731302d059898fa71
Webb orbit <130279128+Webb-orbit@users.noreply.github.com>
2025-03-30 11:47:55 +0530

    Update Setting.jsx

--- src/compos/dashbord/Setting.jsx
+++ src/compos/dashbord/Setting.jsx
@@ -4,13 +4,13 @@ const Setting = () => {
   const [count, setCount] = useState(0);
 
 return (
-  <div className='poppins-regular w-[90%] mx-auto p-7 rounded-md h-[100%] bg-neutral-950  mb-4 max-[800px]:flex-col max-[800px]:w-[95%] max-[800px]:px-1 max-[800px]:py-7'>
+  <div className='poppins-regular w-[90%] mx-auto p-7 rounded-md h-[100%] bg-black  mb-4 max-[800px]:flex-col max-[800px]:w-[95%] max-[800px]:px-1 max-[800px]:py-7'>
       <div className=' mt-4 mb-4'>
         <h2 className=' uppercase text-[1.2rem] tracking-[2px] text-neutral-200 select-none font-bold'>account Settings</h2>
         <p className='text-[0.7rem] text-neutral-400 font-semibold '>customize your space</p>
       </div>
 
-      <div className='mb-4 p-4 bg-neutral-950/50'>
+      <div className='mb-4 p-4 bg-neutral-950'>
         <h3 className='capitalize text-[1.1rem] text-neutral-400 select-none font-bold'>home</h3>
         <p className='text-[0.7rem] text-neutral-400 font-semibold '>customize your space</p>
         
