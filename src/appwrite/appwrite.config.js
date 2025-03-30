be836f8d98950869e5deb8dacfd358c8018df3e8
Webb orbit <130279128+Webb-orbit@users.noreply.github.com>
2025-03-30 13:49:04 +0530

    Add admin id

--- src/appwrite/appwrite.config.js
+++ src/appwrite/appwrite.config.js
@@ -3,6 +3,7 @@ const Appwriteconf = {
     appwriteendpoint: String(import.meta.env.VITE_APPWRITE_ENDPOINT),
     appwritebase: String(import.meta.env.VITE_BASE_ID),
     blogcollid: String(import.meta.env.VITE_BLOG_COLLECTION_ID),
+    admincollid: String(import.meta.env.VITE_ADMIN_COLLECTION_ID),
 }
 
 export default Appwriteconf
