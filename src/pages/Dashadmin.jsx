fa27244264bffea07e82a2efbb092ad592b1fda5
Webb orbit <130279128+Webb-orbit@users.noreply.github.com>
2025-04-08 20:29:48 +0530

    Update Dashadmin.jsx

--- src/pages/Dashadmin.jsx
+++ src/pages/Dashadmin.jsx
@@ -4,17 +4,23 @@ import { Outlet, useNavigate } from 'react-router-dom'
 import Loadingpage from '../compos/utiles/Loadingpage'
 import Navber from '../compos/common/Navber'
 import Dashnav from '../compos/dashbord/Dashnav'
+import { useDispatch} from "react-redux"
+import Admin from "../../appwrite/auth"
+import Settbase from "../../appwrite/Settingapi"
 
 const Dashadmin = () => {
     const [loading, setloading] = useState(true)
     const naviget = useNavigate()
+    const disptch = useDispatch()
 
     useEffect(() => {
         (async () => {
             try {
                 const admin = await Admin.getcurrentaccount()
+                const adminblog = await Settbase.getadmin()
                 if (admin && admin.labels.includes("admin")) {
                     setloading(false)
+                    disptch(storelogin({admindocid: adminblog.documents[0].$id}))
                 } else {
                     naviget("/")
                 }
